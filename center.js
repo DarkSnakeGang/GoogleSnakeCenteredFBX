38cf12129ef7c372d9e9486586c61e518c68e787
Benny Yarmolovich <someonesomewheremax@gmail.com>
2021-08-09 11:49:50 +0300

    Create center.js

---
+++ center.js
@@ -0,0 +1,4 @@
+window.snake.center = function() {
+  document.getElementsByTagName('div')[0].style = 'position:relative;top:50%;transform:translate(0%,-50%);margin:auto;'; 
+};
+window.snake.center();
