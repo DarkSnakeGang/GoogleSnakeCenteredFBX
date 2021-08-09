3b31dd2173a7952d99630941fdba43dca94bf2c1
Benny Yarmolovich <someonesomewheremax@gmail.com>
2021-08-09 12:00:56 +0300

    Update center.js

--- center.js
+++ center.js
@@ -1,4 +1,3 @@
 window.snake.center = function() {
   document.getElementsByTagName('div')[0].style = 'position:relative;top:50%;transform:translate(0%,-50%);margin:auto;'; 
 };
-window.snake.center();
